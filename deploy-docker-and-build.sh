27f9408e93281803d42fcf3f647902c02f5d6703
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-11-22 17:02:36 -0500

    Create deploy-docker-and-build.sh

---
+++ deploy-docker-and-build.sh
@@ -0,0 +1,7 @@
+# run as root for pulseaudio, for X11 forwarding via docker check selinux settings or !!lastly dangeously in work environemnts other security !unsafe -> setenforce 0 
+
+xhost SI:localuser:root
+mkdir -p /home/developer
+
+docker build -t tanya-kate-mod . --no-cache
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/tanya-kate-mod
