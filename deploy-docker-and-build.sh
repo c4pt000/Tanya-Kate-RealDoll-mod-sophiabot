a4f0c9346923b5f669c8dc0eab8ac61aa5b11b17
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-11-24 15:00:18 -0500

    Update deploy-docker-and-build.sh

--- deploy-docker-and-build.sh
+++ deploy-docker-and-build.sh
@@ -1,7 +1,9 @@
 # run as root for pulseaudio, for X11 forwarding via docker check selinux settings or !!lastly dangeously in work environemnts other security !unsafe -> setenforce 0 
 
+# attempt removal of longsheet and black garbage bag talk
+
 xhost SI:localuser:root
 mkdir -p /home/developer
 
 docker build -t tanya-kate . --no-cache
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/tanya-sophiabot-mod
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/tanya-kate-rmzxbr
