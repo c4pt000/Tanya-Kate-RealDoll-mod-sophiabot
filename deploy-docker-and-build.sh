86cf518c41158450734876894dc67b90bf04970e
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2019-11-24 16:32:58 -0500

    Update deploy-docker-and-build.sh

--- deploy-docker-and-build.sh
+++ deploy-docker-and-build.sh
@@ -6,4 +6,4 @@ xhost SI:localuser:root
 mkdir -p /home/developer
 
 docker build -t tanya-kate-removal . --no-cache
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/tanya-longsheet-removing
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/tanya-removal-drugs-mp
