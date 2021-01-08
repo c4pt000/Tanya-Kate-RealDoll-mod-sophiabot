9f489b532fabf35526340308daf0a7ae4d348dee
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-01-08 01:58:37 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/tanya-kate
+docker run -it --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/snd c4pt/tanya-kate-current
+
